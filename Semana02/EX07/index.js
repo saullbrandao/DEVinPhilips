d99cfefd5d8caebf2208cdcb544c2051126c7d6a
Saull Brandão <saull@outlook.com>
2022-10-14 21:30:54 -0300

    refactor

--- Semana02/EX07/index.js
+++ Semana02/EX07/index.js
@@ -1,24 +1,29 @@
 function estacaoAno() {
-    const dataHoje = new Date()
-    const outonoInicio = new Date(`${dataHoje.getFullYear()}/03/22`)
-    const outonoFim = new Date(`${dataHoje.getFullYear()}/06/21`)
-    const invernoInicio = new Date(`${dataHoje.getFullYear()}/06/22`)
-    const invernoFim = new Date(`${dataHoje.getFullYear()}/09/21`)
-    const primaveraInicio = new Date(`${dataHoje.getFullYear()}/09/22`)
-    const primaveraFim = new Date(`${dataHoje.getFullYear()}/12/21`)
-    let estação
+  const dataHoje = new Date()
+  const anoAtual = dataHoje.getFullYear()
 
-    if (dataHoje >= outonoInicio && dataHoje <= outonoFim) {
-        estação = 'Outono'
-    } else if (dataHoje >= invernoInicio && dataHoje <= invernoFim) {
-        estação = 'Inverno'
-    } else if (dataHoje >= primaveraInicio && dataHoje <= primaveraFim) {
-        estação = 'Primavera'
-    } else {
-        estação = 'Verão'
-    }
+  const outonoInicio = new Date(`${anoAtual}/03/22`)
+  const outonoFim = new Date(`${anoAtual}/06/21`)
 
-    alert(`Estação atual no hesmifério sul: ${estação}`)
+  const invernoInicio = new Date(`${anoAtual}/06/22`)
+  const invernoFim = new Date(`${anoAtual}/09/21`)
+
+  const primaveraInicio = new Date(`${anoAtual}/09/22`)
+  const primaveraFim = new Date(`${anoAtual}/12/21`)
+
+  let estação
+
+  if (dataHoje >= outonoInicio && dataHoje <= outonoFim) {
+    estação = 'Outono'
+  } else if (dataHoje >= invernoInicio && dataHoje <= invernoFim) {
+    estação = 'Inverno'
+  } else if (dataHoje >= primaveraInicio && dataHoje <= primaveraFim) {
+    estação = 'Primavera'
+  } else {
+    estação = 'Verão'
+  }
+
+  alert(`Estação atual no hesmifério sul: ${estação}`)
 }
 
 estacaoAno()
