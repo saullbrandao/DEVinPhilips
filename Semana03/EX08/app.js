2b1b4c61750df311a5ddf9cff3270b5c8982aceb
Saull Brandão <saull@outlook.com>
2022-10-18 09:45:01 -0300

    M1S03EX08

---
+++ Semana03/EX08/app.js
@@ -0,0 +1,16 @@
+const uneDobraRetorna = (numerosArray, ...restNumeros) => {
+  const resultArray = [...numerosArray]
+
+  restNumeros.forEach(num => resultArray.push(num * 2))
+
+  return resultArray
+}
+
+const r1 = uneDobraRetorna([1, 2, 3], 4, 4)
+console.log(r1) // [1, 2, 3, 8, 8]
+
+const r2 = uneDobraRetorna([2], 10, 4, 8)
+console.log(r2) // [2, 20, 8, 16]
+
+const r3 = uneDobraRetorna([6, 8])
+console.log(r3) // [6, 8]
