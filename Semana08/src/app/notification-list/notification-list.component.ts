647137e80b87de08974e5e6119d27ae544fd26ce
Saull Brandão <saull@outlook.com>
2022-11-23 07:46:55 -0300

    M1S08EX07

--- Semana08/src/app/notification-list/notification-list.component.ts
+++ Semana08/src/app/notification-list/notification-list.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ntap-notification-list',
   templateUrl: './notification-list.component.html',
-  styleUrls: ['./notification-list.component.scss']
+  styleUrls: ['./notification-list.component.scss'],
 })
 export class NotificationListComponent {
-
+  notifications = [
+    {
+      title: 'Notificação 1',
+      subtitle: 'Lorem ipsum dolor sit amet.',
+      date: new Date('2022/11/22'),
+    },
+    {
+      title: 'Notificação 2',
+      subtitle: 'Lorem ipsum dolor sit amet.',
+      date: new Date('2022/11/20'),
+    },
+    {
+      title: 'Notificação 3',
+      subtitle: 'Lorem ipsum dolor sit amet.',
+      date: new Date('2022/11/19'),
+    },
+  ];
 }
