c120abeeffb6e8a256858a2e862bd570448482d9
Saull Brandão <saull@outlook.com>
2022-11-23 07:40:37 -0300

    M1S08EX06

--- Semana08/src/app/app.module.ts
+++ Semana08/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotificationListComponent
+    NotificationListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
