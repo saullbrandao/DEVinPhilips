3f9b59058207f29717756c1dadace37212700479
Saull Brandão <saull@outlook.com>
2022-11-23 08:00:28 -0300

    M1S08EX09

--- Semana08/src/app/notification-list/notification-list.component.ts
+++ Semana08/src/app/notification-list/notification-list.component.ts
@@ -8,19 +8,40 @@ import { Component } from '@angular/core';
 export class NotificationListComponent {
   notifications = [
     {
+      id: 1,
       title: 'Notificação 1',
       subtitle: 'Lorem ipsum dolor sit amet.',
       date: new Date('2022/11/22'),
+      isRead: false,
     },
     {
+      id: 2,
       title: 'Notificação 2',
       subtitle: 'Lorem ipsum dolor sit amet.',
       date: new Date('2022/11/20'),
+      isRead: false,
     },
     {
+      id: 3,
       title: 'Notificação 3',
       subtitle: 'Lorem ipsum dolor sit amet.',
       date: new Date('2022/11/19'),
+      isRead: false,
     },
   ];
+
+  setReadStatus(id: number) {
+    const changedNotifications = this.notifications.map((notification) => {
+      if (notification.id === id) {
+        return {
+          ...notification,
+          isRead: true,
+        };
+      } else {
+        return notification;
+      }
+    });
+
+    this.notifications = changedNotifications;
+  }
 }
