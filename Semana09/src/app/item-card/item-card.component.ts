946feeb8c1c4bdf4664f28996281a395d1ad038d
Saull Brandão <saull@outlook.com>
2022-11-30 08:31:56 -0300

    M1S09EX05

--- Semana09/src/app/item-card/item-card.component.ts
+++ Semana09/src/app/item-card/item-card.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.css'],
 })
-export class ItemCardComponent {}
+export class ItemCardComponent {
+  character = {
+    name: 'Walter White',
+    img: 'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+    actor: 'Bryan Cranston',
+    description:
+      'Ator, dublador, roteirista, diretor e produtor norte-americano, mais conhecido por interpretar Walter White na série Breaking Bad.',
+  };
+}
