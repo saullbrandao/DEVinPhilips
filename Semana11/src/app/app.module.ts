77254b9bed242144704288854d8f59af3df914e6
Saull Brandão <saull@outlook.com>
2022-12-13 08:22:44 -0300

    M1S11EX01

--- Semana11/src/app/app.module.ts
+++ Semana11/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapitalizePhillipsPipe } from './pipes/capitalize-phillips.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizePhillipsPipe
   ],
   imports: [
     BrowserModule,
---
+++ Semana11/src/app/pipes/capitalize-phillips.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CapitalizePhillipsPipe } from './capitalize-phillips.pipe';
+
+describe('CapitalizePhillipsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CapitalizePhillipsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
