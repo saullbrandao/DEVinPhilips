859296bfd6ec64e81a3d93adeecc385c17aa60d6
Saull Brandão <saull@outlook.com>
2023-02-14 08:58:55 -0300

    M2S03EX2

--- Semana03/src/Clinica.java
+++ Semana03/src/Clinica.java
@@ -12,4 +12,24 @@ public class Clinica {
         this.logradouro = logradouro;
         this.codigo = codigo;
     }
+
+    public Double calcularImc(Double peso, Double altura) {
+        double imc = peso / (altura * altura);
+        imprimeCategoriaImc(imc);
+        return imc;
+    }
+
+    private void imprimeCategoriaImc(double imc) {
+        if(imc < 18.5) {
+            System.out.println("Magreza");
+        } else if(imc < 25d) {
+            System.out.println("Normal");
+        } else if(imc < 30d) {
+            System.out.println("Sobrepeso");
+        } else if(imc < 40d) {
+            System.out.println("Obesidade");
+        } else {
+            System.out.println("Obesidade Grave");
+        }
+    }
 }
--- Semana03/src/Main.java
+++ Semana03/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Clinica clinica = new Clinica("Teste", "Teste", "123123123", "Rua Teste", 1234);
 
+        Double imc = clinica.calcularImc(84.2, 1.88);
+        System.out.println(imc);
     }
 }
