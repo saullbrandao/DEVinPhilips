6d051922bc1002b868810d0659a5d30f767d9011
Saull Brandão <saull@outlook.com>
2023-02-14 08:45:02 -0300

    M2S03EX1

---
+++ Semana03/src/Clinica.java
@@ -0,0 +1,15 @@
+public class Clinica {
+    String nome;
+    String dono;
+    String CNPJ;
+    String logradouro;
+    Integer codigo;
+
+    public Clinica(String nome, String dono, String CNPJ, String logradouro, Integer codigo) {
+        this.nome = nome;
+        this.dono = dono;
+        this.CNPJ = CNPJ;
+        this.logradouro = logradouro;
+        this.codigo = codigo;
+    }
+}
