e8c479d0ceffee6090237be2b6d5f187d313d219
Saull Brandão <saull@outlook.com>
2023-02-23 16:07:49 -0300

    M2S04EX4

---
+++ Semana04/src/EX4/Main.java
@@ -0,0 +1,22 @@
+package EX4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua idade: ");
+        try {
+            int idade = scanner.nextInt();
+            if (idade > 100) throw new RuntimeException("Idade maior que 100");
+            if (idade == 0) throw new RuntimeException("Idade igual a 0");
+            if (idade < 0) throw new RuntimeException("Idade negativa");
+
+            System.out.println("Olá, você tem " + idade + " anos de idade!");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
