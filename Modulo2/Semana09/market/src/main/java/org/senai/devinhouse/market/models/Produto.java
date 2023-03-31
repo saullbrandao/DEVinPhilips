4314fa4d3eb1fc9b488849b68f245ba9d012faa7
Saull Brandão <saull@outlook.com>
2023-03-31 09:23:05 -0300

    M2S09EX07

---
+++ Modulo2/Semana09/market/src/main/java/org/senai/devinhouse/market/models/Produto.java
@@ -0,0 +1,58 @@
+package org.senai.devinhouse.market.models;
+
+import jakarta.persistence.*;
+
+@Entity(name = "produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    @ManyToOne
+    private Categoria categoria;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
