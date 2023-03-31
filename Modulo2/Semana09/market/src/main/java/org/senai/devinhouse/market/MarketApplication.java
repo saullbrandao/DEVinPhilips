7c3271c40649d7365740954067f71d8aebd8a056
Saull Brandão <saull@outlook.com>
2023-03-31 09:09:50 -0300

    M2S09EX05

---
+++ Modulo2/Semana09/market/src/main/java/org/senai/devinhouse/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package org.senai.devinhouse.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+	}
+
+}
---
+++ Modulo2/Semana09/market/src/test/java/org/senai/devinhouse/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.devinhouse.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
