75e83796a48f9314c557f16cc700fe714346e7c7
Saull Brandão <saull@outlook.com>
2023-04-04 14:21:22 -0300

    M2S10

---
+++ Modulo2/Semana10/src/main/java/org/senai/devinhouse/devinphilips/DevinphilipsApplication.java
@@ -0,0 +1,13 @@
+package org.senai.devinhouse.devinphilips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinphilipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinphilipsApplication.class, args);
+	}
+
+}
---
+++ Modulo2/Semana10/src/test/java/org/senai/devinhouse/devinphilips/DevinphilipsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.devinhouse.devinphilips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevinphilipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
