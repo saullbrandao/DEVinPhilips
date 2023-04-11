6dcae082ffbac02f90de5a35b8d2e8d4ca66f400
Saull Brandão <saull@outlook.com>
2023-04-11 19:15:59 -0300

    M2S11

---
+++ Modulo2/Semana11/m2s11/src/main/java/com/example/m2s11/M2s11Application.java
@@ -0,0 +1,13 @@
+package com.example.m2s11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2s11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2s11Application.class, args);
+	}
+
+}
---
+++ Modulo2/Semana11/m2s11/src/test/java/com/example/m2s11/M2s11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m2s11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2s11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
