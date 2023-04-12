00ee5cef73e9e2dbeae2fdb7cf357770c35ad214
Saull Brandão <saull@outlook.com>
2023-04-12 11:05:06 -0300

    M2S11EX02

---
+++ Modulo2/Semana11/m2s11/src/main/java/com/example/m2s11/repositories/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.example.m2s11.repositories;
+
+import com.example.m2s11.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
