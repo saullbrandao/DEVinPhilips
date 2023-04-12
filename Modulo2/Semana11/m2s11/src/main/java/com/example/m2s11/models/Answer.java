b89ef8c715d650e46373001b81a942512b2048b5
Saull Brandão <saull@outlook.com>
2023-04-12 10:59:48 -0300

    M2S11EX01

---
+++ Modulo2/Semana11/m2s11/src/main/java/com/example/m2s11/models/Answer.java
@@ -0,0 +1,21 @@
+package com.example.m2s11.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    @OneToOne
+    private Question question;
+}
---
+++ Modulo2/Semana11/m2s11/src/main/java/com/example/m2s11/models/Question.java
@@ -0,0 +1,21 @@
+package com.example.m2s11.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title, text;
+
+    @ManyToOne
+    private Quiz quiz;
+}
