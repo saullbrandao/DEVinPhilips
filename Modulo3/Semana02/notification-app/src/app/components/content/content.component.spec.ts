d2e58386c48355606f20e74cbef2b94c66c17cb3
Saull Brandão <saull@outlook.com>
2023-05-12 13:22:52 -0300

    M3S02EX02

---
+++ Modulo3/Semana02/notification-app/src/app/components/content/content.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ContentComponent } from './content.component';
+import { NotificationService } from 'src/app/services/notification.service';
+import { AppModule } from 'src/app/app.module';
+
+describe('ContentComponent', () => {
+  let fixture: ComponentFixture<ContentComponent>;
+  let component: ContentComponent;
+  let notificationService = jasmine.createSpyObj(NotificationService, [
+    'editNotificationApi',
+    'getNotifications',
+    'removeNotification',
+  ]);
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppModule],
+      declarations: [ContentComponent],
+      providers: [
+        { provide: NotificationService, useValue: notificationService },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ContentComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
+  });
+});
