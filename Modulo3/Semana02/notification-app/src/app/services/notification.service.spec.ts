2602d1eb54c2e7755b3f3d4920f8672defc7aba3
Saull Brandão <saull@outlook.com>
2023-05-12 13:42:51 -0300

    M3S02EX09

--- Modulo3/Semana02/notification-app/src/app/services/notification.service.spec.ts
+++ Modulo3/Semana02/notification-app/src/app/services/notification.service.spec.ts
@@ -5,6 +5,7 @@ import {
 import { NotificationService } from './notification.service';
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { NOTIFICATIONS_MOCK } from '../utils/notifications-mock';
 
 describe('NotificationService', () => {
   let httpTestingController: HttpTestingController;
@@ -24,5 +25,59 @@ describe('NotificationService', () => {
 
   it('Should create component', () => {
     expect(notificationService).toBeTruthy();
+  });
+
+  it('getNotifications - Should call method with success', () => {
+    notificationService.getNotifications().subscribe((data) => {
+      expect(data).toEqual(NOTIFICATIONS_MOCK);
+    });
+  });
+
+  it('getNotificationsApi - Should call method with success', () => {
+    notificationService.getNotificationsApi().subscribe();
+
+    const request = httpTestingController.expectOne(
+      `${notificationService.BASE_URL}`
+    );
+    expect(request.request.method).toEqual('GET');
+    httpTestingController.verify();
+  });
+
+  it('addNotificationApi - Should call method with success', () => {
+    const notificationMock = { message: 'Test' };
+
+    notificationService.addNotificationApi(notificationMock).subscribe();
+
+    const request = httpTestingController.expectOne(
+      `${notificationService.BASE_URL}`
+    );
+    expect(request.request.method).toEqual('POST');
+    expect(request.request.body).toEqual(notificationMock);
+    httpTestingController.verify();
+  });
+
+  it('editNotificationApi - Should call method with success', () => {
+    const notificationMock = { id: 1, message: 'Test' };
+
+    notificationService.editNotificationApi(notificationMock).subscribe();
+
+    const request = httpTestingController.expectOne(
+      `${notificationService.BASE_URL}/${notificationMock.id}`
+    );
+    expect(request.request.method).toEqual('PUT');
+    expect(request.request.body).toEqual(notificationMock);
+    httpTestingController.verify();
+  });
+
+  it('removeNotification - Should call method with success', () => {
+    const notificationMock = { id: 1, message: 'Test' };
+
+    notificationService.removeNotification(notificationMock.id).subscribe();
+
+    const request = httpTestingController.expectOne(
+      `${notificationService.BASE_URL}/${notificationMock.id}`
+    );
+    expect(request.request.method).toEqual('DELETE');
+    httpTestingController.verify();
   });
 });
