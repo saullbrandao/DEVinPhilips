03eb16cfdfd5c289585248fd36a6689c9024781d
Saull Brandão <saull@outlook.com>
2023-05-12 13:26:37 -0300

    M3S02EX04

--- Modulo3/Semana02/notification-app/src/app/components/content/content.component.spec.ts
+++ Modulo3/Semana02/notification-app/src/app/components/content/content.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContentComponent } from './content.component';
 import { NotificationService } from 'src/app/services/notification.service';
 import { AppModule } from 'src/app/app.module';
+import { of } from 'rxjs';
 
 describe('ContentComponent', () => {
   let fixture: ComponentFixture<ContentComponent>;
@@ -29,11 +30,31 @@ describe('ContentComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('ngOnInit - Should call carregarNotificacoes method with success', () =>{
+  it('ngOnInit - Should call carregarNotificacoes method with success', () => {
     spyOn(component, 'carregarNotificacoes');
     component.ngOnInit();
     expect(component.carregarNotificacoes).toHaveBeenCalled();
-});
+  });
 
+  it('lerNotificacao - Should call atualizarLista method with success', () => {
+    spyOn(component, 'atualizarLista');
+    const notificacaoMock = {
+      aplicativo: '',
+      titulo: '',
+      descricao: '',
+      tempoPublicacao: '',
+      imagem: '',
+      lido: false,
+      id: 1,
+    };
+    const updatedNotificacao = { ...notificacaoMock, lido: true };
+    notificationService.editNotificationApi.and.returnValue(of({}));
 
+    component.lerNotificacao(notificacaoMock);
+
+    expect(notificationService.editNotificationApi).toHaveBeenCalledWith(
+      updatedNotificacao
+    );
+    expect(component.atualizarLista).toHaveBeenCalled();
+  });
 });
